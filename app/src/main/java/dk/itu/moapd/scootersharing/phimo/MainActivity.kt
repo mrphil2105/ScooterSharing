5994cdf36fdcffc78c3710222ad7da7cf32e9510
Philip Mørch <mrphil2105@gmail.com>
2023-02-14 18:53:59 +0100

    Use view bindings instead of findViewById

--- app/src/main/java/dk/itu/moapd/scootersharing/phimo/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/phimo/MainActivity.kt
@@ -1,44 +1,42 @@
 package dk.itu.moapd.scootersharing.phimo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import dk.itu.moapd.scootersharing.phimo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private val TAG = MainActivity::class.qualifiedName
     }
 
-    private lateinit var scooterName: EditText
-    private lateinit var scooterLocation: EditText
-    private lateinit var startRideButton: Button
+    private lateinit var mainBinding: ActivityMainBinding
 
     private val scooter: Scooter = Scooter("", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        scooterName = findViewById(R.id.edit_text_name)
-        scooterLocation = findViewById(R.id.edit_text_location)
-        startRideButton = findViewById(R.id.start_ride_button)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-        startRideButton.setOnClickListener {
-            if (scooterName.text.isNotEmpty() && scooterLocation.text.isNotEmpty()) {
-                val name = scooterName.text.toString().trim()
-                val location = scooterLocation.text.toString().trim()
+        with(mainBinding) {
+            startRideButton.setOnClickListener { view ->
+                if (editTextName.text.isNotEmpty() && editTextLocation.text.isNotEmpty()) {
+                    val name = editTextName.text.toString().trim()
+                    val location = editTextLocation.text.toString().trim()
 
-                scooter.setName(name)
-                scooter.setLocation(location)
+                    scooter.name = name;
+                    scooter.location = location;
 
-                scooterName.text.clear()
-                scooterLocation.text.clear()
+                    editTextName.text.clear()
+                    editTextLocation.text.clear()
 
-                showMessage()
+                    showMessage()
+                }
             }
         }
+
+        setContentView(mainBinding.root)
     }
 
     private fun showMessage() {
