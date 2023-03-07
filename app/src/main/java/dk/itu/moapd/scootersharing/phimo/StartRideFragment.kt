9eb4e5464474a987d596dbb529863c65d8f2551e
Philip Mørch <mrphil2105@gmail.com>
2023-03-07 20:00:05 +0100

    Create StartRideFragment and UpdateRideFragment with same layout

---
+++ app/src/main/java/dk/itu/moapd/scootersharing/phimo/StartRideFragment.kt
@@ -0,0 +1,59 @@
+package dk.itu.moapd.scootersharing.phimo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import dk.itu.moapd.scootersharing.phimo.databinding.FragmentRidesBinding
+
+class StartRideFragment : Fragment() {
+    private lateinit var ridesBinding: FragmentRidesBinding
+
+    companion object {
+        lateinit var ridesDB: RidesDB
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ridesDB = RidesDB.get(requireContext())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        ridesBinding = FragmentRidesBinding.inflate(layoutInflater, container, false)
+        return ridesBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(ridesBinding) {
+            startUpdateRideButton.setOnClickListener {
+                if (editTextName.text.isNotEmpty() && editTextLocation.text.isNotEmpty()) {
+                    val name = editTextName.text.toString().trim()
+                    val location = editTextLocation.text.toString().trim()
+                    ridesDB.addScooter(name, location)
+
+                    val scooter = ridesDB.getCurrentScooter()
+                    showMessage(scooter, view)
+
+                    // TODO: Navigate to 'MainFragment'.
+                }
+            }
+
+            editTextName.isEnabled = true
+        }
+    }
+
+    private fun showMessage(scooter: Scooter, view: View) {
+        val message = "Ride started using scooter ${scooter.name} at location ${scooter.location}."
+        val snackbar = Snackbar.make(view, message, BaseTransientBottomBar.LENGTH_SHORT)
+        snackbar.show()
+    }
+}
---
+++ app/src/main/java/dk/itu/moapd/scootersharing/phimo/UpdateRideFragment.kt
@@ -0,0 +1,50 @@
+package dk.itu.moapd.scootersharing.phimo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dk.itu.moapd.scootersharing.phimo.databinding.FragmentRidesBinding
+
+class UpdateRideFragment : Fragment() {
+    private lateinit var ridesBinding: FragmentRidesBinding
+
+    companion object {
+        lateinit var ridesDB: RidesDB
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ridesDB = RidesDB.get(requireContext())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        ridesBinding = FragmentRidesBinding.inflate(layoutInflater, container, false)
+        return ridesBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(ridesBinding) {
+            startUpdateRideButton.setOnClickListener {
+                if (editTextName.text.isNotEmpty() && editTextLocation.text.isNotEmpty()) {
+                    val location = editTextLocation.text.toString().trim()
+                    ridesDB.updateCurrentScooter(location)
+
+                    // TODO: Navigate to 'MainFragment'.
+                }
+            }
+
+            val scooter = ridesDB.getCurrentScooter()
+            editTextName.setText(scooter.name)
+            editTextLocation.setText(scooter.location)
+            startUpdateRideButton.setText(R.string.update_ride_button)
+        }
+    }
+}
