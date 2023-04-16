ae6df3d79eac6f0faf6835fb1da25cb9aedc2611
Philip Mørch <mrphil2105@gmail.com>
2023-04-16 19:25:59 +0200

    Move touch callback code to separate file

--- app/src/main/java/dk/itu/moapd/scootersharing/phimo/fragments/MainFragment.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/phimo/fragments/MainFragment.kt
@@ -1,27 +1,19 @@
 package dk.itu.moapd.scootersharing.phimo.fragments
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Rect
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import dk.itu.moapd.scootersharing.phimo.R
 import dk.itu.moapd.scootersharing.phimo.adapters.ScooterArrayAdapter
 import dk.itu.moapd.scootersharing.phimo.databinding.FragmentMainBinding
+import dk.itu.moapd.scootersharing.phimo.helpers.ScooterTouchCallback
 import dk.itu.moapd.scootersharing.phimo.models.RidesDB
-import kotlin.math.roundToInt
 
 class MainFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
@@ -51,79 +43,10 @@ class MainFragment : Fragment() {
         return mainBinding.root
     }
 
-    // Swipe feature inspired by article: https://medium.com/getpowerplay/understanding-swipe-and-drag-gestures-in-recyclerview-cb3136beff20
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val displayMetrics: DisplayMetrics = resources.displayMetrics
-        val width = (displayMetrics.widthPixels / displayMetrics.density).toInt().dp
-
-        val deleteIcon =
-            ResourcesCompat.getDrawable(resources, R.drawable.baseline_delete_forever_24, null)
-        val deleteColor = resources.getColor(android.R.color.holo_red_light, null)
-
-        swipeHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-            0, ItemTouchHelper.LEFT
-        ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ) = true
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                AlertDialog.Builder(requireContext())
-                    .setMessage("Are you sure you want to delete the scooter?")
-                    .setPositiveButton(R.string.accept_option) { _, _ ->
-                        val pos = viewHolder.bindingAdapterPosition
-                        ridesDB.removeScooter(pos)
-                        adapter.notifyItemRemoved(pos)
-                    }.setNegativeButton(R.string.decline_option) { _, _ ->
-                        val pos = viewHolder.bindingAdapterPosition
-                        // Used to get rid of the swiped state on the item.
-                        adapter.notifyItemChanged(pos)
-                    }.create().show()
-            }
-
-            override fun onChildDraw(
-                canvas: Canvas,
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                dX: Float,
-                dY: Float,
-                actionState: Int,
-                isCurrentlyActive: Boolean
-            ) {
-                canvas.clipRect(
-                    viewHolder.itemView.left,
-                    viewHolder.itemView.top,
-                    viewHolder.itemView.right,
-                    viewHolder.itemView.bottom
-                )
-
-                val hozMargin = 12.dp
-                val middle = viewHolder.itemView.height / 2
-                val halfHeight = deleteIcon!!.intrinsicHeight / 2
-
-                deleteIcon.bounds = Rect(
-                    width - hozMargin - deleteIcon.intrinsicWidth,
-                    viewHolder.itemView.top + middle - halfHeight,
-                    width - hozMargin,
-                    viewHolder.itemView.top + (middle + halfHeight)
-                )
-
-                when {
-                    dX > -width / 3 -> canvas.drawColor(Color.GRAY)
-                    else -> canvas.drawColor(deleteColor)
-                }
-
-                deleteIcon.draw(canvas)
-
-                super.onChildDraw(
-                    canvas, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive
-                )
-            }
-        })
+        swipeHelper = ItemTouchHelper(ScooterTouchCallback(requireContext(), resources, adapter))
 
         with(mainBinding) {
             startRideButton.setOnClickListener {
@@ -152,9 +75,4 @@ class MainFragment : Fragment() {
             swipeHelper.attachToRecyclerView(scooterList)
         }
     }
-
-    private val Int.dp
-        get() = TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP, toFloat(), resources.displayMetrics
-        ).roundToInt()
 }
---
+++ app/src/main/java/dk/itu/moapd/scootersharing/phimo/helpers/ScooterTouchCallback.kt
@@ -0,0 +1,98 @@
+package dk.itu.moapd.scootersharing.phimo.helpers
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+import android.util.DisplayMetrics
+import android.util.TypedValue
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import dk.itu.moapd.scootersharing.phimo.R
+import dk.itu.moapd.scootersharing.phimo.adapters.ScooterArrayAdapter
+import dk.itu.moapd.scootersharing.phimo.models.RidesDB
+import kotlin.math.roundToInt
+
+// Swipe feature inspired by article: https://medium.com/getpowerplay/understanding-swipe-and-drag-gestures-in-recyclerview-cb3136beff20
+class ScooterTouchCallback(
+    private val context: Context,
+    private val resources: Resources,
+    private val adapter: ScooterArrayAdapter
+) :
+    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+    private val deleteIcon =
+        ResourcesCompat.getDrawable(resources, R.drawable.baseline_delete_forever_24, null)!!
+    private val deleteColor = resources.getColor(android.R.color.holo_red_light, null)
+
+    private val ridesDB = RidesDB.get(context)
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ) = true
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        AlertDialog.Builder(context)
+            .setMessage("Are you sure you want to delete the scooter?")
+            .setPositiveButton(R.string.accept_option) { _, _ ->
+                val pos = viewHolder.bindingAdapterPosition
+                ridesDB.removeScooter(pos)
+                adapter.notifyItemRemoved(pos)
+            }.setNegativeButton(R.string.decline_option) { _, _ ->
+                val pos = viewHolder.bindingAdapterPosition
+                // Used to get rid of the swiped state on the item.
+                adapter.notifyItemChanged(pos)
+            }.create().show()
+    }
+
+    override fun onChildDraw(
+        canvas: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val width = (displayMetrics.widthPixels / displayMetrics.density).toInt().dp
+
+        canvas.clipRect(
+            viewHolder.itemView.left,
+            viewHolder.itemView.top,
+            viewHolder.itemView.right,
+            viewHolder.itemView.bottom
+        )
+
+        val hozMargin = 12.dp
+        val middle = viewHolder.itemView.height / 2
+        val halfHeight = deleteIcon.intrinsicHeight / 2
+
+        deleteIcon.bounds = Rect(
+            width - hozMargin - deleteIcon.intrinsicWidth,
+            viewHolder.itemView.top + middle - halfHeight,
+            width - hozMargin,
+            viewHolder.itemView.top + (middle + halfHeight)
+        )
+
+        when {
+            dX > -width / 3 -> canvas.drawColor(Color.GRAY)
+            else -> canvas.drawColor(deleteColor)
+        }
+
+        deleteIcon.draw(canvas)
+
+        super.onChildDraw(
+            canvas, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive
+        )
+    }
+
+    private val Int.dp
+        get() = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, toFloat(), resources.displayMetrics
+        ).roundToInt()
+}
